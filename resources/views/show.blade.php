480ff61d97b6c353109b5142e4656ece1566ccfb
JasbirCodeSpace <shikhawat.jasbir@gmail.com>
2020-07-14 23:14:42 +0530

    Worked on movie info page

---
+++ resources/views/show.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.main')
+
+@section('content')
+<!-- start movie info  -->
+<div class="movie-info border-b border-gray-800">
+    <div class="container mx-auto px-4 py-16 flex">
+        <img src="/img/parasite.jpg" alt="parasite" class="w-96">
+        <div class="ml-24">
+            <h2 class="text-4xl font-semibold">
+                Parasite(2020)
+            </h2>
+        </div>
+    </div>
+</div>
+<!-- end movie info  -->
+@endsection
