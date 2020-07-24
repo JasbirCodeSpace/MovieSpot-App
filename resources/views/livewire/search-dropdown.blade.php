8046d2af7c978e672f5b774055a44ac774a8aa33
JasbirCodeSpace <shikhawat.jasbir@gmail.com>
2020-07-24 20:05:26 +0530

    Added alpine library and click events handle

--- resources/views/livewire/search-dropdown.blade.php
+++ resources/views/livewire/search-dropdown.blade.php
@@ -1,7 +1,12 @@
-<div class="relative mt-4 md:mt-0">
-    <input wire:model.debounce.500ms="search" type="text"
+<div class="relative mt-4 md:mt-0" x-data="{ isOpen:true }" @click.away="isOpen = false">
+    <input 
+        wire:model.debounce.500ms="search" type="text"
         class="bg-gray-800 text-sm rounded-full w-58 md:w-64 px-4 pl-8 py-1 focus:outline-none focus:shadow-outline"
-        placeholder="Search">
+        placeholder="Search"
+        @focus="isOpen = true" 
+        @keydown = "isOpen = true"
+        @keydown.escape.window = "isOpen = false"
+        @keydown.shift.tab = "isOpen = false">
     <div class="absolute top-0">
         <svg class="fill-current w-4 text-gray-500 mt-2 ml-2" viewBox="0 0 24 24">
             <path class="heroicon-ui"
@@ -10,13 +15,15 @@
     </div>
     <div wire:loading class="spinner top-0 right-0 mt-3 mr-4"></div>
     @if( strlen($search) >= 2)
-        <div class="absolute bg-gray-800 text-sm rounded w-64 mt-4">
+        <div class="z-50 absolute bg-gray-800 text-sm rounded w-64 mt-4" 
+                x-show.transition.opacity="isOpen">
             @if($searchResults->count() > 0 )
                 <ul>
                     @foreach($searchResults as $result)
                         <li class="border-b border-gray-700">
                             <a href="{{ route('movies.show', $result['id']) }}"
-                                class="block hover:bg-gray-700 px-3 py-3 flex items-center">
+                                class="block hover:bg-gray-700 px-3 py-3 flex items-center"
+                                @if($loop->last)@keydown.tab= "isOpen = false" @endif>
                         @if($result['poster_path'])
                                 <img src="https://image.tmdb.org/t/p/w92/{{ $result['poster_path'] }}"
                          alt="" class="w-4">
