ac199337198a6f92bab08e00db59eec324c904ea
JasbirCodeSpace <shikhawat.jasbir@gmail.com>
2020-08-23 23:58:22 +0530

    purgecss

--- app/Http/Livewire/SearchDropdown.php
+++ app/Http/Livewire/SearchDropdown.php
@@ -9,7 +9,8 @@ class SearchDropdown extends Component
 {
     public $search = '';
     public $searchType = '';
-    public function mount($page){
+    public function mount($page)
+    {
         $this->searchType = explode('/', $page)[0];
     }
     public function render()
@@ -17,10 +18,9 @@ class SearchDropdown extends Component
         $searchResults = [];
 
         if (strlen($this->search) >= 2) {
-                $searchResults = Http::withToken(config('services.tmdb.token'))
-                    ->get(config('services.tmdb.base_url') . '/search/multi/?query=' . $this->search)
-                    ->json()['results'];
-            
+            $searchResults = Http::withToken(config('services.tmdb.token'))
+                ->get(config('services.tmdb.base_url') . '/search/movie/?query=' . $this->search)
+                ->json()['results'];
         }
 
         return view('livewire.search-dropdown', [
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,5 @@
-const mix = require('laravel-mix');
+const mix = require("laravel-mix");
+require("laravel-mix-purgecss");
 
 /*
  |--------------------------------------------------------------------------
@@ -11,7 +12,4 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-mix.postCss('resources/css/main.css', 'public/css', [
-    require('tailwindcss'),
-])
+mix.postCss("resources/css/main.css", "public/css", [require("tailwindcss")]).purgeCss();
